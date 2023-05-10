fa835f041745432fded28ad4809c7635e9086c47
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-05-10 22:01:54 +0200

    added errors in app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import {catchErrors,notFoundError} from './middlewares/errors.js';
 const app = express();
 
-
+app.use(notFoundError);
+app.use(catchErrors);
 export default app;
